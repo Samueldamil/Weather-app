9844b482605c3897a5e14a480d762e878e19a69d
Samueldamil <damilareedun@gmail>
2025-04-07 04:24:38 +0100

    Make adjustment

--- server.js
+++ server.js
@@ -39,5 +39,5 @@ app.get('/weather', async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server starting is running on http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
